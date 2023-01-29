36dc75b61e9670bf72002c6e7bfe6323485852ac
mateusfmelo <mateusmelo51@gmail.com>
2023-01-29 20:08:13 -0300

    Add files via Upload

---
+++ aula13/while.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 2)
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`);
+    c++
+}
+*/
+
+/*
+console.log('Tudo Bem?');
+console.log('Tudo Bem?');
+console.log('Tudo Bem?');
+console.log('Tudo Bem?');
+console.log('Tudo Bem?');
+*/
